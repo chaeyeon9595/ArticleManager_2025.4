a13646bf8e1f8ec88b70280f105909e5563625cf
cjang5189@gmail.com <cjang5189@gmail.com>
2025-04-23 21:32:38 +0900

    loginedMember, isLogined()를 Controller로 이전해서 ArticleController, MemberController 다 사용하도록

--- src/main/java/koreaIT/controller/Controller.java
+++ src/main/java/koreaIT/controller/Controller.java
@@ -1,7 +1,15 @@
 
 package koreaIT.controller;
 
+import koreaIT.dto.Member;
+
 public abstract class Controller {
+
+    static Member loginedMember = null;
+
+    protected boolean isLogined(){
+        return loginedMember != null;
+    }
 
     public abstract void doAction(String methodName, String cmd);
 
