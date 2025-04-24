bf5de91903e3ead26232ec169bee07dd4f79f45f
cjang5189@gmail.com <cjang5189@gmail.com>
2025-04-24 21:41:03 +0900

    Controller, Service, Dao 구조로 리팩토링 중

---
+++ src/main/java/koreaIT/service/ArticleService.java
@@ -0,0 +1,21 @@
+
+package koreaIT.service;
+
+import koreaIT.Container;
+import koreaIT.dao.ArticleDao;
+import koreaIT.dto.Article;
+
+import java.util.List;
+
+public class ArticleService {
+
+    private ArticleDao articleDao;
+
+    public ArticleService() {
+        articleDao = Container.articleDao;
+    }
+
+    public List<Article> getArticle(){
+        return articleDao.getArticleList();
+    }
+}
